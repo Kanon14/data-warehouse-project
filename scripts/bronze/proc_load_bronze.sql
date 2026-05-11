9853d7a27a90af31e62279a1c85a6a022797365e
Kanon14 <geochua144@gmail.com>
2026-05-11 17:02:23 +0800

    edit error loading in bronze layer procedure

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -32,8 +32,15 @@ BEGIN
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_cust_info';
 		TRUNCATE TABLE bronze.crm_cust_info;
+
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
-		
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
@@ -78,7 +85,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -93,7 +100,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -108,7 +115,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\Users\cjx14\Personal_Projects\data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',

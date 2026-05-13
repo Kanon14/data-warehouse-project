80b11c108e6cb8486b5c6f95169ddfc6781ed882
Kanon14 <geochua144@gmail.com>
2026-05-13 22:46:37 +0800

    added gold layer scripts

--- scripts/procedure_exec.sql
+++ scripts/procedure_exec.sql
@@ -1,3 +1,8 @@
 -- To execute the procedures in bronze and silver layer
 bronze.load_bronze
 silver.load_silver
+
+-- To observe view in gold layer
+SELECT * FROM gold.dim_customers
+SELECT * FROM gold.dim_products
+SELECT * FROM gold.fact_sales
